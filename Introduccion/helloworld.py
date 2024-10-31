51844c1a8f932f88b071b732120c6ff896442bc8
alvaro.eng <alvaro.eng.98@gmail.com>
2024-10-31 00:05:06 -0400

    variables de python

---
+++ Introduccion/helloworld.py
@@ -0,0 +1,34 @@
+#PRIMER DIA DE RESUMEN DE PYTHON
+# ESTO ES UN COMENTARIO
+'''
+ESTO TAMBIEN ES UN COMENTARIO
+'''
+
+#imprimiendo texto en pantalla
+print('Hola Mundo con python el dia 1')
+
+#OPERACCIONES FUNDAMENTALES CON PYTHON
+print('Suma 2 + 3')
+print(2 + 3)
+print('Resta 2 - 1')
+print(2 - 1)
+print('Multiplicacion 2 * 2')
+print(2 * 2)
+print('Division 4 / 2')
+print(4 / 2)
+print('Exponencial 4**2 significa 4 a la 2')
+print(4**2)
+print('Modulo 3 % 2 -- es decir es lo que queda resultante de la division')
+print(15 % 6)
+print('Division entera 6 // 2 es decir lo que se multiplica por el 2')
+print(6 // 2)
+
+#TIPOS
+print(type(10))                     #Int
+print(type(3.14))                   #Float
+print(type(1 + 3j))                 #Numero Complejo
+print(type('Canada'))               #Cadena de Texto
+print(type([1, 2, 3]))              #Lista
+print(type({'nombre':'Canada'}))    #Diccionario
+print(type({9.8, 3.14, 2}))         #Set
+print(type((9.8, 3.14, 2)))         #Tuplas
---
+++ VARIABLES-FUNCIONES-EXISTENTES/variables.py
@@ -0,0 +1,33 @@
+#Funciones Existentes en Python
+
+print('Funcion len() devuelve el tamaño')
+print('Funcion type() devuelve el tipo de dato')
+print('Funciones int(), float(), str(), castea el dato al tipo de dato seleccionado entero, float, string')
+
+
+
+#VARIABLES DE PYTHON
+
+first_name = 'Nombre'
+last_name = 'Apellido'
+country = 'Pais'
+city = 'Ciudad'
+age = 26
+is_married = True
+skills = ['Python', 'Docker', 'Kurbenetes', 'Linux']
+personal_info = {
+    'first_name' : 'Nombre',
+    'last_name' : 'Apellido',
+    'country' : 'Pais',
+    'city' : 'Ciudad',
+}
+
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+
+
+#DECLARANDO VARIABLES EN UNA SOLA LINEA
+first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
