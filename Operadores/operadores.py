0fa5c3c8acd2491249af34f13ab04bc4b820c07b
alvaro.eng <alvaro.eng.98@gmail.com>
2024-11-04 07:14:43 -0500

    Operadores iniciales

---
+++ Operadores/operadores.py
@@ -0,0 +1,10 @@
+#Listado de operaciones que existen en python
+
+print('Operaciones')
+print('Adicion/suma -- ', 1 +2 )
+print('Resta -- ', 3 - 1)
+print('Multiplicacion -- ', 4 * 2)
+print('Division -- ', 4 / 2) #la division devuelve un numero float
+print('Division quedandose con el resto -- ', 7 // 2) #aqui me quedo con lo que multiplico por 2
+print('Modulo --', 5 % 3) # me quedo con el resultado del multiplo de 3 - 5 o sea 5 - 3(1) = 2
+print('Exponencial --', 3**2)
